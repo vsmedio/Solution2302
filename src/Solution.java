cb2de9379ffadebf611d9194fcf0c1f48eea2279
Arvis Abromovics <info@vsmedio.lv>
2018-01-12 22:08:20 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,45 @@
+/* 
+Запрети переопределение
+Запрети переопределение метода onMouseDown.
+
+
+Требования:
+1. Класс Listener должен быть создан внутри класса Solution.
+2. Класс Listener должен быть публичным.
+3. Метод onMouseDown должен быть публичным.
+4. Должна быть запрещена возможность переопределить метод onMouseDown.
+
+public class Solution {
+
+    public static class Listener {
+        public void onMouseDown(int x, int y) {
+            //do something on mouse down event
+        }
+
+        public void onMouseUp(int x, int y) {
+            //do something on mouse up event
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+*/
+
+public class Solution {
+
+    public static class Listener {
+        public final void onMouseDown(int x, int y) {
+            //do something on mouse down event
+        }
+
+        public void onMouseUp(int x, int y) {
+            //do something on mouse up event
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
